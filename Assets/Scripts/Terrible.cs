a23e87ef99a5b1ce1f127ec446005f306e8d1ecd
KalleIlvonen <kalle.ilvonen@student.eduko.fi>
2026-03-02 13:28:33 +0200

    Added a terrible

---
+++ Assets/Scripts/Terrible.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Terrible : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("This is terrible");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
